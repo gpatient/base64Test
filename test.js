6f469de96d10600d3236430271aafb83342c5736
gpatient <gpatient@naver.com>
2015-10-19 01:31:03 +0800

    save10

--- test.js
+++ test.js
@@ -6,6 +6,6 @@ import speech from './index.js';
 
 export function dsp(t)
 {
-  return speech.dsp(t);
-  //return Math.sin(300*t*3.141592*2);
+  //return speech.dsp(t);
+  return Math.sin(300*t*3.141592*2);
 }
