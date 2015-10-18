9f490152fedce404287c742f5280caa7e6f56f50
gpatient <gpatient@naver.com>
2015-10-19 01:36:22 +0800

    save11

--- test.js
+++ test.js
@@ -3,9 +3,10 @@
  * test
  */
 import speech from './index.js';
+var speecha = speech(); 
 
 export function dsp(t)
 {
-  //return speech.dsp(t);
-  return Math.sin(300*t*3.141592*2);
+  return speecha.qwer.dsp(t);
+  //return Math.sin(300*t*3.141592*2);
 }
