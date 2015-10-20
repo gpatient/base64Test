2e3b06bc0ec47f575a679b18ecf4c54abb2a80e4
gpatient <gpatient@naver.com>
2015-10-21 06:35:46 +0900

    save30

--- test.js
+++ test.js
@@ -12,7 +12,7 @@ dbg('asdf7 ')(qqq.rrrr);
 
 export function dsp(t)
 {
-  return qqq.dsp(t);
-  //return qqq.speechStr('pu po bo to ko ga fa');
+  //return qqq.dsp(t);
+  return qqq.speechStr('pu po bo to ko ga fa');
   //return Math.sin(300*t*3.141592*2);
 }
